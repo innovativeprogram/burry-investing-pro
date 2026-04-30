3a2f1569ea8fba2eaa210a57d5c3394239a3b031
innovativeprogram <innovativeprogram@proton.me>
2026-04-30 12:33:04 +0200

    Update BurryInvestingPro.py

--- BurryInvestingPro.py
+++ BurryInvestingPro.py
@@ -2,7 +2,7 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import numpy as np
-import pandas_ta as ta
+import pandas-ta as ta
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import re
